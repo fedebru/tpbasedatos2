f3ac4e3bad13a5ea01de452a67ff48704e00e5c4
Fede Bru <fedebru@gmail.com>
2018-04-16 20:55:37 -0300

    sp delete turno

---
+++ Desarrollo/procedure_delete_turno.sql
@@ -0,0 +1,17 @@
+CREATE PROCEDURE dbo.spDELETE_dbo_turno @id int
+AS
+
+
+SET NOCOUNT ON
+
+DELETE FROM [dbo].[turno]
+         WHERE
+           (@id
+		              )
+GO
+
+/*
+EXECUTE [dbo].[dbo.spDELETE_dbo_turno] 
+   @id =1
+GO
+*/
